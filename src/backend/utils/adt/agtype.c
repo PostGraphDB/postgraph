03105ebb094613efc8e22aab0795723376a2bd9c
Josh Innis <JoshInnis@gmail.com>
2023-06-04 22:21:44 +0900

    Refactor ceil and floor

--- src/backend/utils/adt/agtype.c
+++ src/backend/utils/adt/agtype.c
@@ -5237,99 +5237,58 @@ PG_FUNCTION_INFO_V1(age_ceil);
 
 Datum age_ceil(PG_FUNCTION_ARGS)
 {
-    int nargs;
-    Datum *args;
-    bool *nulls;
-    Oid *types;
-    agtype_value agtv_result;
-    Numeric arg;
-    Numeric numeric_result;
-    float8 float_result;
+    agtype *agt = AG_GET_ARG_AGTYPE_P(0);
     bool is_null = true;
 
-    /* extract argument values */
-    nargs = extract_variadic_args(fcinfo, 0, true, &args, &types, &nulls);
-
-    /* check number of args */
-    if (nargs != 1)
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-                        errmsg("ceil() invalid number of arguments")));
-
-    /* check for a null input */
-    if (nargs < 0 || nulls[0])
+    if (is_agtype_null(agt))
         PG_RETURN_NULL();
 
-    /*
-     * ceil() supports integer, float, and numeric or the agtype integer,
-     * float, and numeric for the input expression.
-     */
-    arg = get_numeric_compatible_arg(args[0], types[0], "ceil", &is_null, NULL);
+    if (is_agtype_numeric(agt)) {
+        Numeric arg = get_numeric_compatible_arg(AGTYPE_P_GET_DATUM(agt), AGTYPEOID, "ceil", &is_null, NULL);
 
-    /* check for a agtype null input */
-    if (is_null)
-        PG_RETURN_NULL();
+        Numeric result = DatumGetNumeric(DirectFunctionCall1(numeric_ceil, NumericGetDatum(arg)));
 
-    /* We need the input as a numeric so that we can pass it off to PG */
-    numeric_result = DatumGetNumeric(DirectFunctionCall1(numeric_ceil,
-                                                         NumericGetDatum(arg)));
+        agtype_value agtv = { .type = AGTV_NUMERIC, .val.numeric = result };
 
-    float_result = DatumGetFloat8(DirectFunctionCall1(numeric_float8_no_overflow,
-                                                      NumericGetDatum(numeric_result)));
-    /* build the result */
-    agtv_result.type = AGTV_FLOAT;
-    agtv_result.val.float_value = float_result;
+        AG_RETURN_AGTYPE_P(agtype_value_to_agtype(&agtv));
+    } else {
+        float8 arg = get_float_compatible_arg(AGTYPE_P_GET_DATUM(agt), AGTYPEOID, "ceil", &is_null);
+    
+        float8 result = DatumGetFloat8(DirectFunctionCall1(dceil, Float8GetDatum(arg)));
+    
+        agtype_value agtv = { .type = AGTV_FLOAT, .val.float_value = result };
 
-    PG_RETURN_POINTER(agtype_value_to_agtype(&agtv_result));
+        AG_RETURN_AGTYPE_P(agtype_value_to_agtype(&agtv));
+    }
 }
 
 PG_FUNCTION_INFO_V1(age_floor);
 
 Datum age_floor(PG_FUNCTION_ARGS)
 {
-    int nargs;
-    Datum *args;
-    bool *nulls;
-    Oid *types;
-    agtype_value agtv_result;
-    Numeric arg;
-    Numeric numeric_result;
-    float8 float_result;
+    agtype *agt = AG_GET_ARG_AGTYPE_P(0);
     bool is_null = true;
 
-    /* extract argument values */
-    nargs = extract_variadic_args(fcinfo, 0, true, &args, &types, &nulls);
-
-    /* check number of args */
-    if (nargs != 1)
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-                        errmsg("floor() invalid number of arguments")));
-
-    /* check for a null input */
-    if (nargs < 0 || nulls[0])
+    if (is_agtype_null(agt))
         PG_RETURN_NULL();
 
-    /*
-     * floor() supports integer, float, and numeric or the agtype integer,
-     * float, and numeric for the input expression.
-     */
-    arg = get_numeric_compatible_arg(args[0], types[0], "floor", &is_null,
-                                     NULL);
+    if (is_agtype_numeric(agt)) {
+        Numeric arg = get_numeric_compatible_arg(AGTYPE_P_GET_DATUM(agt), AGTYPEOID, "floor", &is_null, NULL);
 
-    /* check for a agtype null input */
-    if (is_null)
-        PG_RETURN_NULL();
+        Numeric result = DatumGetNumeric(DirectFunctionCall1(numeric_floor, NumericGetDatum(arg)));
+                                          
+        agtype_value agtv = { .type = AGTV_NUMERIC, .val.numeric = result };
 
-    /* We need the input as a numeric so that we can pass it off to PG */
-    numeric_result = DatumGetNumeric(DirectFunctionCall1(numeric_floor,
-                                                         NumericGetDatum(arg)));
+        AG_RETURN_AGTYPE_P(agtype_value_to_agtype(&agtv));
+    } else {
+        float8 arg = get_float_compatible_arg(AGTYPE_P_GET_DATUM(agt), AGTYPEOID, "floor", &is_null);
+    
+        float8 result = DatumGetFloat8(DirectFunctionCall1(dfloor, Float8GetDatum(arg)));
+    
+        agtype_value agtv = { .type = AGTV_FLOAT, .val.float_value = result };
 
-    float_result = DatumGetFloat8(DirectFunctionCall1(numeric_float8_no_overflow,
-                                                      NumericGetDatum(numeric_result)));
-    /* build the result */
-    agtv_result.type = AGTV_FLOAT;
-    agtv_result.val.float_value = float_result;
-
-    PG_RETURN_POINTER(agtype_value_to_agtype(&agtv_result));
+        AG_RETURN_AGTYPE_P(agtype_value_to_agtype(&agtv));
+    }
 }
 
 PG_FUNCTION_INFO_V1(age_abs);
