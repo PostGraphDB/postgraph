cc281475fa7aaee9e481ad33ebd4fa30162eba7e
Josh Innis <JoshInnis@gmail.com>
2023-06-25 13:53:29 -0500

    Allow Partial Routes to be added to Routes
    Allow the Partial  Route datatype to be added to a path in place
    of an edge

--- regress/sql/route.sql
+++ regress/sql/route.sql
@@ -56,3 +56,23 @@ SELECT build_route(
         build_vertex('5'::graphid, $$vertex_label$$, agtype_build_map())
 );
 
+
+SELECT build_route(
+        build_vertex('2'::graphid, $$vertex_label$$, agtype_build_map()),
+        build_partial_route(
+		build_edge('1'::graphid, '2'::graphid, '3'::graphid, $$edge_label$$, agtype_build_map())
+	),
+        build_vertex('3'::graphid, $$vertex_label$$, agtype_build_map())
+);
+
+SELECT build_route(
+        build_vertex('2'::graphid, $$vertex_label$$, agtype_build_map()),
+        build_partial_route(
+            build_edge('1'::graphid, '2'::graphid, '3'::graphid, $$edge_label$$, agtype_build_map()),
+            build_vertex('3'::graphid, $$vertex_label$$, agtype_build_map()),
+            build_edge('4'::graphid, '3'::graphid, '5'::graphid, $$edge_label$$, agtype_build_map())
+        ),
+        build_vertex('3'::graphid, $$vertex_label$$, agtype_build_map())
+);
+
+
