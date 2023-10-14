10cdf372f491cc24e56c57a46f50bbfe163053f6
Josh Innis <JoshInnis@gmail.com>
2023-10-13 22:23:35 -0500

    Implement regr_sxx

--- postgraph--0.1.0.sql
+++ postgraph--0.1.0.sql
@@ -4049,6 +4049,24 @@ CREATE AGGREGATE covar_samp(gtype, gtype) (
     parallel = SAFE
 );
 
+CREATE FUNCTION gtype_regr_sxx_final(float8[])
+RETURNS gtype
+LANGUAGE c
+IMMUTABLE
+CALLED ON NULL INPUT
+PARALLEL SAFE
+AS 'MODULE_PATHNAME';
+
+CREATE AGGREGATE regr_sxx(gtype, gtype) (
+    stype = float8[],
+    sfunc = gtype_regr_accum,
+    finalfunc = gtype_regr_sxx_final,
+    combinefunc = float8_regr_combine,
+    finalfunc_modify = READ_ONLY,
+    initcond = '{0,0,0,0,0,0}',
+    parallel = SAFE
+);
+
 CREATE FUNCTION vle (IN gtype, IN vertex, IN vertex, IN gtype, IN gtype,
                      IN gtype, IN gtype, IN gtype, OUT edges variable_edge)
 RETURNS SETOF variable_edge 
